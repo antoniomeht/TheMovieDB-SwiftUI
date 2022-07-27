c0d98aa87fef9dc1c26912a7266e074474ed3cf1
antoniomeht <antonio.meht@gmail.com>
2022-07-27 07:38:06 +0200

    Initial Commit

---
+++ TheMovieDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovieDB
+//
+//  Created by Antonio Araez Moreno on 27/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Antonio Araez Moreno on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
