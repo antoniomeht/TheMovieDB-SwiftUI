d62d2b4a642aa557fd33dbda91f49dbfb93c4fef
Antonio <antonio.meht@gmail.com>
2022-07-28 13:59:48 +0200

    ImageLoader & MovieCard

---
+++ TheMovieDB/Utils/ImageLoader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageLoader.swift
+//  TheMovieDB
+//
+//  Created by Antonio Araez Moreno on 28/7/22.
+//
+
+import Foundation
---
+++ TheMovieDB/Views/MovieCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCard.swift
+//  TheMovieDB
+//
+//  Created by Antonio Araez Moreno on 28/7/22.
+//
+
+import SwiftUI
+
+struct MovieCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCard()
+    }
+}
